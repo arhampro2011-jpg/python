e9312677d0f12f63c6d32fd62e79fd75fea3e6c5
arhampro2011-jpg <arhampro2011@gmail.com>
2026-07-19 18:08:11 +0530

    task complete

--- homework/fib.py
+++ homework/fib.py
@@ -1,12 +1,11 @@
-total=0
-
 def fib(num):
     if num ==1:
         return 0
     elif num==2:
         return 1
-    total=total+fib(num+1)
-    return total
+    else:
+        pass 
+    return fib(num-1)+fib(num-2)
 a=int(input("input number"))
 print(fib(a))
     
